b584dbe1820812fb8f90d81fef590e2508bec993
Sushovan <sushovanghosh960@gmail.com>
2024-01-22 22:14:22 +0530

    first challenge

---
+++ src/challenges/Challenge1.jsx
@@ -0,0 +1,16 @@
+
+export const ReactComponent = (
+    <div>
+        <img src="./react-logo.png" width="40px" />
+        <h1>Fun facts about React</h1>
+
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by John Walke</li>
+            <li>Has well over 100k stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, includes mobile apps. </li>
+
+        </ul>
+    </div>
+);
---
+++ src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { ReactComponent } from './challenges/Challenge1.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(ReactComponent)
