5eb3847d854903e4c5dee7e59db382c9b1dfa1a7
Sushovan <sushovanghosh960@gmail.com>
2024-01-26 20:58:27 +0530

    complete additional challenge
    Break all the element into three different component
    Compose those components in Page
    Explore child and parent components and establish relation between them.
    This could be an example of React's composability

--- src/challenges/challenge2.jsx
+++ src/challenges/challenge2.jsx
@@ -10,27 +10,47 @@ Challenge:
     "© 20xx <last name here> development. All rights reserved."
  */
 
+function Header() {
+    return (
+        <header>
+            <nav>
+                <img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png" alt="react-logo" width="40px" />
+            </nav>
+        </header>
+    )
+}
+
+function MainComponent() {
+    return (
+        <>
+            <h1>Reasons I'm excited to learn React</h1>
+
+            <ol>
+                <li>It's a popular library, so I'll be
+                    able to fit in with the cool kids!</li>
+                <li>I'm more likely to get a job as a developer
+                    if I know React</li>
+            </ol>
+
+        </>
+    )
+};
+
+function Footer() {
+    return (
+        <footer>
+            <p>© 2024 Ghosh development. All rights reserved.</p>
+        </footer>
+    )
+};
+
+
 export default function Page() {
     return (
         <>
-            <header>
-                <nav>
-                    <img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png" alt="react-logo" width="40px" />
-
-                    <h1>Reasons I'm excited to learn React</h1>
-
-                    <ol>
-                        <li>It's a popular library, so I'll be
-                            able to fit in with the cool kids!</li>
-                        <li>I'm more likely to get a job as a developer
-                            if I know React</li>
-                    </ol>
-                </nav>
-            </header>
-
-            <footer>
-                <p>© 2024 Ghosh development. All rights reserved.</p>
-            </footer>
+            <Header />
+            <MainComponent />
+            <Footer />
         </>
     )
 }
