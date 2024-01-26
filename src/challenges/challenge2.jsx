427c4e02fa5f5b72cd0aa45119856b395331c5e8
Sushovan <sushovanghosh960@gmail.com>
2024-01-26 20:47:02 +0530

    solve quiz and challenge
    Add solutioin for challenge2
    Add solution for quizes from Lesson 19.Custom component quiz!"
    "

---
+++ src/challenges/challenge2.jsx
@@ -0,0 +1,36 @@
+/**
+Challenge:  
+- Add a `header` element with a nested `nav` element. Inside the `nav`,
+  include a `img` element with the image of the React logo inside
+  (src="./react-logo.png") and make sure to set the width to something
+  more manageable so it doesn't take up the whole screen
+- Add an `h1` with some text describing the page. (E.g. "Reasons
+  I'm excited to learn React"). Place it above the ordered list.
+- Add a `footer` after the list that says: 
+    "© 20xx <last name here> development. All rights reserved."
+ */
+
+export default function Page() {
+    return (
+        <>
+            <header>
+                <nav>
+                    <img src="https://w7.pngwing.com/pngs/79/518/png-transparent-js-react-js-logo-react-react-native-logos-icon-thumbnail.png" alt="react-logo" width="40px" />
+
+                    <h1>Reasons I'm excited to learn React</h1>
+
+                    <ol>
+                        <li>It's a popular library, so I'll be
+                            able to fit in with the cool kids!</li>
+                        <li>I'm more likely to get a job as a developer
+                            if I know React</li>
+                    </ol>
+                </nav>
+            </header>
+
+            <footer>
+                <p>© 2024 Ghosh development. All rights reserved.</p>
+            </footer>
+        </>
+    )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { ReactComponent } from './challenges/Challenge1.jsx'
+import Page from './challenges/challenge2.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<ReactComponent />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Page />)
