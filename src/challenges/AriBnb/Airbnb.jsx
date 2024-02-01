ce8c5fcfce9539377e0e146208b9f3261a1ce45c
Sushovan <sushovanghosh960@gmail.com>
2024-02-01 21:42:29 +0530

    add hero section component

--- src/challenges/AriBnb/Airbnb.jsx
+++ src/challenges/AriBnb/Airbnb.jsx
@@ -1,9 +1,20 @@
 import AirBnbHeader from "./AirBnbHeader";
+import HeroComponent from "./Hero";
 import '../../css/Airbnb.css';
+
 export default function AirBnbApp() {
     return (
         <>
             <AirBnbHeader />
+
+            <main>
+                <HeroComponent />
+            </main>
         </>
     );
 };
+
+/**
+ * TODO: Add poppins font
+ * 
+ */
---
+++ src/challenges/AriBnb/Hero.jsx
@@ -0,0 +1,16 @@
+export default function HeroComponent() {
+    return (
+        <section id="hero-section">
+
+            <div className="image-container">
+
+                <img className="hero-image" src="./img/airbnb-hero_image.png" alt="airbnb multiple activities image" />
+            </div>
+
+            <div className="text-container">
+                <h1>Online Experiences</h1>
+                <p>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+            </div>
+        </section>
+    )
+}
